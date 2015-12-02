7e1f4f45927d1aa5b0d8b8275d110ae110639672
Sarah Ghidalia <Mileem@MacBook-Air-de-Sarah.local>
2015-12-02 11:59:43 +0100

    package Client - main class of editor

---
+++ src/client/Editor.java
@@ -0,0 +1,35 @@
+package client;
+
+import java.util.HashMap;
+
+import command.*;
+import observer.BufferObserver;
+import receiver.EngineEditionImplemantation;
+
+/**
+ * 
+ * @author Mileem
+ *
+ */
+public class Editor {
+	private static HashMap<String, Command> commands;
+
+	public static void main(String[] args) throws Exception {
+		BufferObserver humanMachineInterface = new BufferObserver();
+		EngineEditionImplemantation engineEdition = new EngineEditionImplemantation();
+		engineEdition.addObserver(humanMachineInterface);
+		
+		//Commands
+		commands = new HashMap<String, Command>();
+		commands.put("copy", new Copy(engineEdition));
+		commands.put("past", new Past(engineEdition));
+		commands.put("cut", new Cut(engineEdition));
+		commands.put("enter", new Enter(engineEdition, humanMachineInterface));
+		commands.put("select", new Select(engineEdition, humanMachineInterface));
+		commands.put("delete", new Delete(engineEdition));
+		commands.put("save", new Save(engineEdition));
+		
+		humanMachineInterface.invokeCommands(commands);
+	}
+
+}
