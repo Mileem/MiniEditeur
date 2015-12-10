258d2dcea087a8ad7083daeee698cf12644fb011
Sarah Ghidalia <Mileem@MacBook-Air-de-Sarah.local>
2015-12-10 20:03:27 +0100

    Add Observer

--- src/observer/BufferObserver.java
+++ src/observer/BufferObserver.java
@@ -6,13 +6,18 @@ import java.util.Observer;
 import invoker.HumanMachineInterface;
 import receiver.EngineEditionImplemantation;
 
+/**
+ * Observed changes in Buffer
+ * @author Mileem
+ *
+ */
 public class BufferObserver extends HumanMachineInterface implements Observer {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	/**
+	 * Update mainContent in HumainMachineInterface when the buffer changes
+	 */
 	@Override
 	public void update(Observable o, Object arg) {
 		if(o instanceof EngineEditionImplemantation) {
